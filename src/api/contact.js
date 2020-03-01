c54c774af900f8f70b20da685b066051445bfa5d
jiaran-web <1534272666@qq.com>
2020-03-01 16:59:37 +0800

    改1

--- src/api/contact.js
+++ src/api/contact.js
@@ -1,1 +1,7 @@
-import require from '@/utils/require'
+import require from '@/utils/require';
+
+export function getContact(oParams){
+  return require.get('/get/contact', {
+    params: oParams.params
+  });
+};
