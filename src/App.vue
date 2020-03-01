d10872981874bf4e815d6b311d6ff506df17da80
jiaran-web <1534272666@qq.com>
2020-03-01 14:07:53 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app" >
+    <app-header></app-header>
+    <app-main></app-main>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+//import '@/assets/css/css.css';
+import appHeader from '@/components/AppHeader';
+import appMain from '@/components/AppMain';
+import appFooter from '@/components/AppFooter';
+
+export default {
+  name: 'App',
+  data(){//储存数据
+    return {
+
+    };
+  },
+  methods: {//储存方法
+
+  },
+  components: {//注册组建
+    appHeader,//appHeader: appHeader,
+    appFooter,
+    appMain,
+  },
+}
+</script>
+
+<style lang="scss">
+@import './assets/css/css.css';
+</style>
